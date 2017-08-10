5faaeaea8ba8cda3ef6e462b0aa311ab100465b5
carvilsi <carvilsi.mail.list@gmail.com>
2017-08-10 17:19:33 +0200

    updating docker stuff

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf lib
 npm run prepublish
-docker build -t sysunite/word-microservice:1.0.2 .
+docker build -t sysunite/word-microservice:1.0.3 .
--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin bash
 
-docker build -t sysunite/word-microservice:1.0.2 .
-docker run --name word-microservice -p 9268:9268 -t sysunite/word-microservice:1.0.2
+docker build -t sysunite/word-microservice:1.0.3 .
+docker run --name word-microservice -p 9268:9268 -t sysunite/word-microservice:1.0.3
